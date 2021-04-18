d57afcae5190ecf1830bd135215df1fb3ceaebba
leo <hireedleo@gmail.com>
2021-04-18 14:43:02 +0800

    modified code and added Makefile

--- pwn/ctf_wiki/stack/ret2dlresolve/hitcon-2015-quals-blinkroot/demoCloseAllFd.c
+++ pwn/ctf_wiki/stack/ret2dlresolve/hitcon-2015-quals-blinkroot/demoCloseAllFd.c
@@ -4,15 +4,36 @@
 */
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
+
+#define OPTSTR "anc:"
 
 int main(int argc, char** argv)
 {
-	if (argc > 1) {
-		for (int i = 0; i < 3; ++i)
-			close(i);
+	int opt;
+	if (argc < 2) {
+			fprintf(stderr, "Usage: %s [-a close all] [-c fd] [-n close none]\n", argv[0]);
+			exit(EXIT_FAILURE);
 	}
-
+	while ((opt = getopt(argc, argv, OPTSTR)) != -1) {
+		switch (opt) {
+		case 'c':
+			close(atoi(optarg));
+			break;
+		case 'a':
+			close(0);
+			close(1);
+			close(2);
+			break;
+		case 'n':
+			/* close none */
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-a close all] [-c fd] [-n close none]\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
 	system("/bin/sh");
 	return 0;
 }
