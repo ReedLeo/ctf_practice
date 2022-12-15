cd0d20244988549545d9e84c94398477bd165cd6
leo <hireedleo@gmail.com>
2022-12-15 17:09:51 +0800

    show the difference between rt_sigreturn and sigreturn

--- pwn/ctf_wiki/stack/ROP/SROP/sigreturnDemo/sigreturnDemo.c
+++ pwn/ctf_wiki/stack/ROP/SROP/sigreturnDemo/sigreturnDemo.c
@@ -9,15 +9,33 @@ void handler(int sig)
     printf("Outch!!\n");
 }
 
+void rt_handler(int sig, siginfo_t* si, void* ucontext) {
+    printf("Caught signal: %d\n", sig);
+}
+
 int main(int argc, char** argv)
 {
     char buf[0x101] = {0};
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
+    struct sigaction sa;
+
+    sa.sa_sigaction = rt_handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigfillset(&sa.sa_mask);
+    
+    printf("The minimum real-time signal number=%d\n", SIGRTMIN);
+    if (sigaction(SIGRTMIN, &sa, NULL) == -1) {
+        perror("sigaction failed.");
+        exit(-1);
+    }
+
+    // setvbuf(stdin, NULL, _IONBF, 0);
+    // setvbuf(stdout, NULL, _IONBF, 0);
     signal(SIGINT, handler);
-    printf("Hi, input a string pattern.\n");
-    scanf("%256s", buf);
+
+    printf("Wait for the signal arrvie.\n");
+    // scanf("%256s", buf);
+    
     pause();
-    printf("What you've input:\n%s", buf);
+    // printf("What you've input:\n%s", buf);
     return 0;
 }
